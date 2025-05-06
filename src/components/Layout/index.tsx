2324d7cb29dea70089117a3ef86c99d9acb6c1eb
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-05-06 11:16:50 -0700

    Refactor Layout component structure and improve header navigation

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,18 +5,49 @@ import { LiaUserFriendsSolid } from 'react-icons/lia'
 import { IoSettingsOutline } from 'react-icons/io5'
 
 
-
 function Layout() {
   return (
     <>
-      <div className='flex flex-col h-screen w-screen bg-gray-100 dark:bg-gray-900'>
-        <main className='flex-grow p-4 bg-Background'>
+      <div className='flex flex-col h-screen w-full bg-gray-100 dark:bg-gray-900'>
+        <header className='shrink-0 flex items-center justify-between p-5  bg-Secondary text-white'>
+          <h1 className='text-2xl md:ml-16'>
+            <Link to='/'>
+              Goodllery
+            </Link>
+          </h1>
+
+          <nav className='hidden md:flex space-x-10 mr-16'>
+            <Link to='/' className='flex flex-col items-center justify-center text-2xl hover:text-Primary'>
+              <BsImages />
+              <span className='text-xs'>Gallery</span>
+            </Link>
+            <Link to='/about' className='flex flex-col items-center justify-center text-2xl hover:text-Primary'>
+              <LiaUserFriendsSolid />
+              <span className='text-xs'>About</span>
+            </Link>
+            <Link to='/contact' className='flex flex-col items-center justify-center text-2xl hover:text-Primary'>
+              <IoSettingsOutline />
+              <span className='text-xs'>Settings</span>
+            </Link>
+          </nav>
+        </header>
+        <main className='flex-grow overflow-y-auto p-4 bg-Background'>
           <Outlet />
         </main>
-        <footer className='grid grid-cols-3 bg-Secondary h-20 items-center justify-items-center text-center text-white dark:bg-SecondaryDark'>
-          <Link to='/' className='text-Background'><BsImages className='text-3xl hover:scale-110 transition-transform duration-150' /></Link>
-          <Link to='/' className='text-Background'><LiaUserFriendsSolid className='text-3xl hover:scale-110 transition-transform duration-150' /></Link>
-          <Link to='/' className='text-Background'><IoSettingsOutline className='text-3xl hover:scale-110 transition-transform duration-150' /></Link>
+
+        <footer className='shrink-0 grid grid-cols-3 bg-Secondary h-20 items-center justify-items-center text-center text-white md:hidden'>
+          <Link to='/' className='flex flex-col items-center justify-center text-2xl hover:text-Primary'>
+            <BsImages />
+            <span className='text-xs'>Gallery</span>
+          </Link>
+          <Link to='/about' className='flex flex-col items-center justify-center text-2xl hover:text-Primary'>
+            <LiaUserFriendsSolid />
+            <span className='text-xs'>About</span>
+          </Link>
+          <Link to='/contact' className='flex flex-col items-center justify-center text-2xl hover:text-Primary'>
+            <IoSettingsOutline />
+            <span className='text-xs'>Settings</span>
+          </Link>
         </footer>
       </div>
     </>
